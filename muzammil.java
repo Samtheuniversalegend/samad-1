e7f86eb2c018dd3357b508f578cc3efa2b3a90e5
Samtheuniversalegend <sam090uu@gmail.com>
2024-10-22 15:59:29 +0500

    samad commit

---
+++ muzammil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shaikh;
+
+/**
+ *
+ * @author se22f-060
+ */
+public class muzammil {
+    public static void main(String[] args) {
+        System.out.println("hello jaba lab1");
+    }
+    
+}
